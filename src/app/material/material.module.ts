2f7a4dc11a87fff1a1781f9963b4076c24f83c33
Alan David Richards <alrichards@incomm.com>
2018-02-16 18:08:23 -0500

    Working to get the input to look like the comp.... Struggling. heh heh

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 //Materials
-import { MatButtonModule, MatToolbarModule, MatGridListModule, MatInputModule } from '@angular/material';
+import { 
+    MatButtonModule, MatToolbarModule, MatGridListModule, MatInputModule, MatCardModule,
+    MatListModule 
+        } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatFormFieldModule, MatInputModule],
+    imports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatCardModule, MatListModule],
+    exports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatFormFieldModule, MatInputModule, MatCardModule, MatListModule],
 })
 
 export class MaterialModule { }
